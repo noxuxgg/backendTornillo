3dbaf3204310783b635d2dabb2c0b1baa58f1d16
ADICTTT <choquecallatadriana@gmail.com>
2026-06-29 01:07:13 -0400

    cORRECION PARA COMPILAR

--- src/modules/proveedores/proveedores.service.ts
+++ src/modules/proveedores/proveedores.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Proveedor } from './entities/proveedore.entity';
-import { CreateProveedorDto } from './dto/create-proveedore.dto';
+import { CreateProveedorDto } from './dto/create-proveedor.dto';
 
 @Injectable()
 export class ProveedoresService {
